7d037536a4043a35a72ba99cba3052d434317027
collinskoech11 <collinskoechck34@gmail.com>
2021-08-23 10:46:16 +0300

    added stack navigator

--- rnauth/App.js
+++ rnauth/App.js
@@ -1,9 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import LoginScreen from './screens/LoginScreen';
-import DashboardScreen from './screens/DashboardScreen';
-import AuthLoading from './screens/AuthLoading';
 
 export default function App() {
   return (
--- rnauth/routes.js
+++ rnauth/routes.js
@@ -1,2 +1,24 @@
-import createAppContainer from 'react-navigation'
-import {createStackNavigator} from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
+import DashboardScreen from './screens/DashboardScreen';
+import AuthLoading from './screens/AuthLoading';
+
+
+const BeforeSignin = createNativeStackNavigator({
+    Login: {
+        screen: LoginScreen
+    }
+}, {
+    headerMode: "none",
+    initialRouteName: "Login"
+})
+
+const AfterSignin = createNativeStackNavigator({
+    Dashboard: {
+        screen: DashboardScreen
+    }
+}, {
+    headerMode: "none",
+    initialRouteName: "Dashboard"
+})
